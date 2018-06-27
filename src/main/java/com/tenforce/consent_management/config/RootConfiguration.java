1ce7d66ba4aedb91a9d8dc1b33e72e6e86152373
Wouter Dullaert <wouter.dullaert@tenfoce.com>
2018-06-27 12:11:33 +0200

    Only inject stuff once

--- src/main/java/com/tenforce/consent_management/config/RootConfiguration.java
+++ src/main/java/com/tenforce/consent_management/config/RootConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = {})
+//@ComponentScan(basePackageClasses = {})
 public class RootConfiguration {
 }
