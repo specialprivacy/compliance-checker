53497c045ddd89aac80bfcf2b1861808984350a4
Wouter Dullaert <wouter.dullaert@tenfoce.com>
2018-07-04 16:18:48 +0200

    KafkaProducer brings its own Threads, no need to make it itself a Thread

--- src/main/java/com/tenforce/consent_management/Main.java
+++ src/main/java/com/tenforce/consent_management/Main.java
@@ -2,7 +2,6 @@ package com.tenforce.consent_management;
 
 import com.tenforce.consent_management.config.Configuration;
 import com.tenforce.consent_management.kafka.ApplicationLogConsumer;
-import com.tenforce.consent_management.kafka.CheckedComplianceLogProducer;
 import com.tenforce.consent_management.kafka.PolicyConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,24 +16,20 @@ public class Main {
 
     private static PolicyConsumer policyConsumer = null;
     private static ApplicationLogConsumer applicationLogConsumer = null;
-    private static CheckedComplianceLogProducer checkedComplianceLogProducer = null;
 
     public static void main(String[] args) {
         log.info("Compliance checker starting up");
         try {
             policyConsumer = new PolicyConsumer(Configuration.getKafkaTopicPolicy());
-            checkedComplianceLogProducer = new CheckedComplianceLogProducer(Configuration.getKafkaTopicConsent(), false);
-            applicationLogConsumer = new ApplicationLogConsumer(Configuration.getKafkaTopicAccess());
-            applicationLogConsumer.setCheckedComplianceLogProducer(checkedComplianceLogProducer);
+            applicationLogConsumer = new ApplicationLogConsumer(Configuration.getKafkaTopicAccess(), Configuration.getKafkaTopicConsent());
 
-            ExecutorService executor = Executors.newFixedThreadPool(2);
+            final ExecutorService executor = Executors.newFixedThreadPool(2);
             executor.submit(policyConsumer);
             executor.submit(applicationLogConsumer);
 
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 log.info("Received request to stop. Gracefully terminating all kafka clients");
                 applicationLogConsumer.shutdown();
-                checkedComplianceLogProducer.interrupt();
                 policyConsumer.shutdown();
                 executor.shutdown();
                 try {
--- src/main/java/com/tenforce/consent_management/kafka/ApplicationLogConsumer.java
+++ src/main/java/com/tenforce/consent_management/kafka/ApplicationLogConsumer.java
@@ -10,6 +10,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.errors.WakeupException;
 import org.rocksdb.RocksDBException;
 import org.semanticweb.owlapi.model.OWLClassExpression;
@@ -23,77 +24,91 @@ import java.util.Properties;
 
 /**
  * Created by langens-jonathan on 4/25/18. This is basic Kafka consumer that listens to the
- * application logs topic and takes them of one by one.
+ * application logs consumerTopic and takes them of one by one.
  *
  * After that it checks the consent for every access.
  */
 public class ApplicationLogConsumer  implements Runnable {
     private final KafkaConsumer<String, String> consumer;
-    private final String topic;
+    private final KafkaProducer<String, String> producer;
+    private final String consumerTopic;
+    private final String producerTopic;
     private static final Logger log = LoggerFactory.getLogger(ApplicationLogConsumer.class);
 
-    private CheckedComplianceLogProducer checkedComplianceLogProducer = null;
     private final ComplianceChecker complianceChecker = new ComplianceChecker(
             new HermiTReasonerFactory(),
             Configuration.getRulesDirectory()
     );
     private final PolicyStore policyStore = PolicyStore.getInstance();
 
-    public ApplicationLogConsumer(String topic) throws RocksDBException, OWLOntologyCreationException {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Configuration.getKafkaURLList());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, Configuration.getKafkaClientID());
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+    public ApplicationLogConsumer(String consumerTopic, String producerTopic) throws RocksDBException, OWLOntologyCreationException {
+        Properties consumerProps = new Properties();
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Configuration.getKafkaURLList());
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, Configuration.getKafkaClientID());
+        consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProps.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        consumerProps.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        Properties producerProps = new Properties();
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Configuration.getKafkaURLList());
+        producerProps.put(ProducerConfig.CLIENT_ID_CONFIG, Configuration.getKafkaClientID());
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        consumer = new KafkaConsumer<>(props);
-        this.topic = topic;
+        consumer = new KafkaConsumer<>(consumerProps);
+        producer = new KafkaProducer<>(producerProps);
+        this.consumerTopic = consumerTopic;
+        this.producerTopic = producerTopic;
     }
 
     @Override
     public void run() {
         try {
-            consumer.subscribe(Collections.singletonList(this.topic));
+            consumer.subscribe(Collections.singletonList(this.consumerTopic));
 
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(1000);
                 for (ConsumerRecord<String, String> record : records) {
-                    log.debug("Processing (topic: {}, partition: {}, offset: {})", this.topic, record.partition(), record.offset());
-                    if(this.checkedComplianceLogProducer != null) {
-                        ObjectMapper mapper = new ObjectMapper();
-                        try {
-                            ApplicationLog alog= mapper.readValue(record.value(), ApplicationLog.class);
-                            OWLClassExpression logClass = alog.toOWL();
-                            OWLClassExpression policyClass = policyStore.getPolicy(alog.getUserID());
-                            alog.setHasConsent(policyClass != null && complianceChecker.hasConsent(logClass, policyClass));
-                            this.checkedComplianceLogProducer.sendMessage(alog.getEventID(), mapper.writeValueAsString(alog));
-                        } catch (IOException e) {
-                            log.error("Failed to parse kafka message");
-                            e.printStackTrace();
-                        } catch (RocksDBException e) {
-                            // TODO: we should probably die here, because without rocksdb we cannot sucessfully process the message
-                            log.error("Failed to read policy from rocksdb");
-                            e.printStackTrace();
-                        }
+                    log.debug("Processing (topic: {}, partition: {}, offset: {})", this.consumerTopic, record.partition(), record.offset());
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        ApplicationLog alog= mapper.readValue(record.value(), ApplicationLog.class);
+                        OWLClassExpression logClass = alog.toOWL();
+                        OWLClassExpression policyClass = policyStore.getPolicy(alog.getUserID());
+                        alog.setHasConsent(policyClass != null && complianceChecker.hasConsent(logClass, policyClass));
+                        producer.send(
+                                new ProducerRecord<>(producerTopic, alog.getEventID(), mapper.writeValueAsString(alog)),
+                                (recordMetadata, e) -> {
+                                    if (null != e) {
+                                        // TODO: we should die in this case especially once we ask kafka to retry
+                                        log.error("Failed to write record {}", e);
+                                        return;
+                                    }
+                                    // TODO: we should manually commit our offset here
+                                    log.debug("Sucessfully written record (topic: {}, partition: {}, offset: {})", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
+                                }
+                        );
+                    } catch (IOException e) {
+                        log.error("Failed to parse kafka message");
+                        e.printStackTrace();
+                    } catch (RocksDBException e) {
+                        // TODO: we should probably die here, because without rocksdb we cannot sucessfully process the message
+                        log.error("Failed to read policy from rocksdb");
+                        e.printStackTrace();
                     }
                 }
             }
         } catch (WakeupException e) {
             // Ignore for shutdown
         } finally {
-            log.info("Closing kafka consumer");
+            log.info("Closing kafka clients");
             consumer.close();
-            log.info("Done closing kafka consumer");
+            producer.close();
+            log.info("Done closing kafka clients");
         }
-    }
-
-    public void setCheckedComplianceLogProducer(CheckedComplianceLogProducer checkedComplianceLogProducer) {
-        this.checkedComplianceLogProducer = checkedComplianceLogProducer;
     }
 
     /**
